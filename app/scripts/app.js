55447ca6835b0effb05f44f10c3343e067414896
kingflurkel <mikethuy@gmail.com>
2016-06-02 10:25:10 +0200

    ...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -65,6 +65,11 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     app.search = !app.search;
   };
 
+  app.gotoClip = function(e){
+    console.log(e.detail);
+    page('/clip/'+e.detail);
+  };
+
   app.host = 'http://109.123.70.141:8545';
 
 })(document);
