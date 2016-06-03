f27802c173d6c4a1151b4fc30fdef2b4eec9520e
Stefaan Ponnet <stefaan@ponnet.com>
2016-06-04 00:46:27 +0200

    hardcoded host again until further notice....

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,7 +28,9 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
-    //Excess.RouteManager.transitionTo('/home');
+    if (window.location.hash === ""){
+      Excess.RouteManager.transitionTo('/home');
+    }
 
 
     app.host = new Polymer.IronMetaQuery({
